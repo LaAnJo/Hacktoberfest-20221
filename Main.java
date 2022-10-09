4897783d63b0a0ac508cbb6c7050c44b67d38bb4
Umairlari786 <uamir.lari1998@gmail.com>
2022-10-09 23:02:00 +0530

    A java program to push all zero at the END.

---
+++ Main.java
@@ -0,0 +1,26 @@
+
+public class Main {
+    public static void main(String[] args)
+    {
+        int[] A = { 5, 6, 0, 4, 6, 0, 9, 0, 8 };
+        int n = A.length;
+        int j = 0;
+        for (int i = 0; i < n; i++) {
+            if (A[i] != 0) {
+                swap(A, j, i);
+                j++;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            System.out.print(A[i] + " ");
+        }
+    }
+
+
+    public static void swap(int[] A, int a, int b)
+    {
+        int temp = A[a];
+        A[a] = A[b];
+        A[b] = temp;
+    }
+}
